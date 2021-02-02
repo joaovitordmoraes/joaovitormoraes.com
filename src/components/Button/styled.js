25beb31a28fa8e2b696a7a5756b43ebc34a946fa
João Moraes <joaovitordmoraes@gmail.com>
2021-02-01 23:01:07 -0300

    :sparkles: Create button component

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -8,7 +8,7 @@ export const ButtonStyle = styled(Link)`
     box-sizing: border-box;
     border: 2px solid var(--color-two);
     font-family: var(--font-krona);
-    font-size: var(--size-xsmall);
+    font-size: var(--size-small);
     color: var(--color-white);
     text-decoration: none;
     text-align: center;
@@ -16,9 +16,31 @@ export const ButtonStyle = styled(Link)`
     transition: all 150ms linear;
 
     &:hover {
-        border: 2px solid var(--color-five);
-        background-color: var(--color-five);
+        border: 2px solid var(${props => props.external ? '--color-one' : '--color-five'});
+        background-color: var(${props => props.external ? '--color-one' : '--color-five'});
         color: var(--color-one);
+        transition: all 150ms linear;
+    }
+`
+
+export const ExternalButtonStyle = styled.a`
+    max-width: 222px;
+    width: 100%;
+    padding: 14px;
+    box-sizing: border-box;
+    border: 2px solid var(--color-two);
+    font-family: var(--font-krona);
+    font-size: var(--size-small);
+    color: var(--color-white);
+    text-decoration: none;
+    text-align: center;
+    display: block;
+    transition: all 150ms linear;
+
+    &:hover {
+        border: 2px solid var(${props => props.external ? '--color-three' : '--color-five'});
+        background-color: var(${props => props.external ? '--color-three' : '--color-five'});
+        color: var(${props => props.external ? '--color-white' : '--color-one'});
         transition: all 150ms linear;
     }
 `
