44a714eca843aff30b09835daaf29ec96c6766dc
Joao <joaovitordmoraes@gmail.com>
2021-02-25 00:48:02 -0300

    :wrench: Update config in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,47 +5,47 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   // Ensures we are processing only markdown files
-  if (node.internal.type === "MarkdownRemark") {
+  if (node.internal.type === 'MarkdownRemark') {
     // Use `createFilePath` to turn markdown files in our `data/faqs` directory into `/faqs/slug`
     const slug = createFilePath({
       node,
       getNode,
-      basePath: "pages",
+      basePath: 'pages'
     })
 
     // Creates new query'able field with name of 'slug'
     createNodeField({
       node,
-      name: "slug",
-      value: `/${slug.slice(12)}`,
+      name: 'slug',
+      value: `/${slug.slice(12)}`
     })
   }
 }
 
 exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions
+  const { createPage } = actions
 
-    return graphql(`
-        {
-            allMarkdownRemark {
-                edges {
-                    node {
-                        fields {
-                            slug
-                        }
-                    }
-                }
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
             }
+          }
         }
-    `).then(result => {
-        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-            createPage({
-                path: node.fields.slug,
-                component: path.resolve('./src/templates/blog-post.js'),
-                context: {
-                    slug: node.fields.slug
-                }
-            })
-        })
+      }
+    }
+  `).then((result) => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve('./src/templates/blog-post.js'),
+        context: {
+          slug: node.fields.slug
+        }
+      })
     })
+  })
 }
